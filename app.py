65541d902920447027d2cc1a1c1ae3797c09d351
bx-NT <bx-nt@bx.kmutt.ac.th>
2022-02-17 17:33:22 +0700

    add threaded

--- app.py
+++ app.py
@@ -25,4 +25,4 @@ app.secret_key = "Miyawaki Sakura"
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=False, port=5000)
+    app.run(threaded=True, port=5000)
